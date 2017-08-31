02c9dc43a4e7930cd0e2cc91535ff4422246cab6
moeabdol <mohd.a.saed@gmail.com>
2017-08-31 08:40:18 +0300

    Implement basic home.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", redirectTo: "", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
