8df4f1d71fd5d001d10b1a31e10ac725e9308244
moeabdol <mohd.a.saed@gmail.com>
2017-09-01 03:06:08 +0300

    Refactor signup.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
@@ -23,7 +23,6 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  FormGroup,
-  FormControl,
-  Validators
-} from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "signup",
@@ -11,28 +7,19 @@ import {
   styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
-  public signupForm:    FormGroup;
-  public username:      FormControl;
-  public email:         FormControl;
-  public password:      FormControl;
-  public confirmation:  FormControl;
+  public signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
-    this.username = new FormControl("", Validators.required);
-    this.email = new FormControl("", [
-      Validators.required,
-      Validators.email
-    ]);
-    this.password = new FormControl("", Validators.required);
-    this.confirmation = new FormControl("", Validators.required);
-
     this.signupForm = new FormGroup({
-      username: this.username,
-      email: this.email,
-      password: this.password,
-      confirmation: this.confirmation
+      username: new FormControl("", Validators.required),
+      email: new FormControl("", [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl("", Validators.required),
+      confirmation: new FormControl("", Validators.required)
     });
   }
 
